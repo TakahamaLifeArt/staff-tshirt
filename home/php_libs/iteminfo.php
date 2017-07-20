fc2c3d1306615e2eb5d9f4068e5ecce7604aa60d
Ks <ks.desk@gmail.com>
2017-07-20 23:06:45 +0900

    update 簡単見積もりのタグ指定を変更

--- home/php_libs/iteminfo.php
+++ home/php_libs/iteminfo.php
@@ -266,7 +266,7 @@ class ItemInfo extends Conndb {
 						),
 						array(
 							40=>array('tag'=>19,'label'=>'パンツ'),
-							43=>array('tag'=>108,'label'=>'シャツ')
+							43=>array('tag'=>101,'label'=>'シャツ')
 						),
 						array(
 							32=>array('tag'=>83,'label'=>'全アイテム')
